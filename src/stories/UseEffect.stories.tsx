579ff523efb0361f70da29352fbb899ee320fee6
Volodymyr Yaremchak <vovkahorror@gmail.com>
2022-12-04 00:05:47 +0200

    cpmpleted the first UseEffect stories

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -5,15 +5,28 @@ export default {
 };
 
 export const Example1 = () => {
+    const [fake, setFake] = useState(1);
     const [counter, setCounter] = useState(1);
 
     useEffect(() => {
+        console.log('useEffect every render');
+        document.title = counter.toString();
+    });
 
-    })
+    useEffect(() => {
+        console.log('useEffect only first render (componentDidMount)');
+        document.title = counter.toString();
+    }, []);
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter change');
+        document.title = counter.toString();
+    }, [counter]);
 
     return (
         <>
-            Hello, {counter}
+            Hello, {counter} {fake}
+            <button onClick={() => setFake(fake + 1)}>+</button>
             <button onClick={() => setCounter(counter + 1)}>+</button>
         </>
     );
