00810e6331587c176fbb794ba0a2d78e411ffd11
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-01-17 03:34:06 +0200

    completed the fourth useEffect stories

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -32,7 +32,7 @@ export const SimpleExample = () => {
     );
 };
 
-export const SetTimeoutExample = () => {
+export const SetIntervalExample = () => {
     const [fake, setFake] = useState(1);
     const [counter, setCounter] = useState(1);
 
@@ -48,9 +48,13 @@ export const SetTimeoutExample = () => {
     const [seconds, setSeconds] = useState(date.getSeconds());
 
     useEffect(() => {
-        setInterval(() => {
+        const intervalID = setInterval(() => {
             setSeconds(state => state < 59 ? state + 1 : 0);
         }, 1000);
+
+        return () => {
+            clearInterval(intervalID);
+        };
     }, []);
 
     return (
@@ -59,6 +63,75 @@ export const SetTimeoutExample = () => {
             <div>{hours}:{minutes}:{seconds}</div>
             <button onClick={() => setFake(fake + 1)}>+</button>
             <button onClick={() => setCounter(counter + 1)}>+</button>
+        </>
+    );
+};
+
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(1);
+
+    console.log('Component rendered');
+
+    useEffect(() => {
+        console.log('Effect occurred');
+
+        return () => {
+            console.log('Reset Effect');
+        };
+    }, []);
+
+    return (
+        <>
+            Hello, counter: {counter}
+            <button onClick={() => setCounter(counter + 1)}>+</button>
+        </>
+    );
+};
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState('');
+
+    console.log('Component rendered');
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key);
+            setText(text + e.key);
+        };
+
+        window.addEventListener('keypress', handler);
+
+        return () => {
+            window.removeEventListener('keypress', handler);
+        };
+    }, [text]);
+
+    return (
+        <>
+            Typed text: {text}
+        </>
+    );
+};
+
+export const SetTimeoutExample = () => {
+    const [text, setText] = useState('');
+
+    console.log('Component rendered');
+
+    useEffect(() => {
+        const timeoutID = setTimeout(() => {
+            console.log('Timeout expired');
+            setText('3 seconds passed');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timeoutID);
+        };
+    }, [text]);
+
+    return (
+        <>
+            Typed text: {text}
         </>
     );
 };
